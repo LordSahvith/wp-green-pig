3fd26f7498bb1afb8bff1d519fff089e9d87178c
Caleb <bulletproofchild@gmail.com>
2019-07-07 11:14:14 -0600

    add underscores files

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * The Green Pig Pub Theme Customizer
+ *
+ * @package The_Green_Pig_Pub
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function the_green_pig_pub_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'the_green_pig_pub_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'the_green_pig_pub_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'the_green_pig_pub_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function the_green_pig_pub_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function the_green_pig_pub_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function the_green_pig_pub_customize_preview_js() {
+	wp_enqueue_script( 'the-green-pig-pub-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'the_green_pig_pub_customize_preview_js' );
